01f3d7cc5a8a94e00c964d762412cd06b5c87e58
Preethy <samueljohnjoseph272@gmail.com>
2025-06-28 15:39:26 +0530

    .

---
+++ Style.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("CMBC Website Loaded Successfully.");
+  });
+  console.log("Welcome to Christ Mission Bible College website!");
+  
